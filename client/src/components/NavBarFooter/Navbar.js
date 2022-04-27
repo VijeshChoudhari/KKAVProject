e05f4399e14f76e186d5ce5138b76a6614d2978c
Tripathiaman2511 <tripathi.aman.official01@gmail.com>
2022-04-27 22:14:54 +0530

    Done

--- client/src/components/NavBarFooter/Navbar.js
+++ client/src/components/NavBarFooter/Navbar.js
@@ -1,8 +1,8 @@
 import React, { useContext,useEffect } from 'react'
-
 import {NavLink} from 'react-router-dom'
 import { UserContext } from '../../App'
 import styles from './Navbar.module.css'
+import block from '../../images/block.svg'
 export const Navbar = () => {
   const {state,dispatch}=useContext(UserContext);
  
@@ -10,29 +10,36 @@ export const Navbar = () => {
     if(state===true){
 
       return(
-        <>
+        <><div className={styles.blockarea}>
+           <NavLink  to="/"><img className={styles.image} src={block} alt="logo" /></NavLink> {" "}
         <div className={styles.Navbar}>
 
-        <NavLink className={styles.a} to="/">Logo</NavLink> {" "}
+        
         <NavLink className={styles.a} to="/">Home</NavLink>  {" "}
         <NavLink className={styles.a} to="/Profile">Profile</NavLink>  {" "}
         {/* <NavLink className={styles.a} to="/contact">Contact</NavLink>   {" "} */}
         <NavLink className={styles.rightnav+' '+styles.a} to="/logout">Logout</NavLink>
         </div>
+        </div>
+       
         </>
       )
     }else{
       return(
         <>
+        <div className={styles.blockarea}>
+        <NavLink  to="/home"><img className={styles.image} src={block} alt="logo" /></NavLink> {" "}
         <div className={styles.Navbar}>
 
-        <NavLink className={styles.a} to="/home">Logo</NavLink> {" "}
+        
         <NavLink className={styles.a} to="/home">Home</NavLink>  {" "}
       {/*   <NavLink className={styles.a} to="/contact">Contact</NavLink>  {" "} */}
        
         <NavLink className={styles.rightnav+' '+styles.a} to="/login">Login</NavLink>
         </div>
     
+        </div>
+        
         </>
       )
     }
